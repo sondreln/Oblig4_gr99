f1ee84194bdf7d0b22da4a4adddd45da65116d48
Sondre <sondrelangedal.ness@gmail.com>
2023-11-05 21:06:43 +0100

    css

--- src/main/java/no/hvl/dat108/Oblig4_gr99/controllers/PaameldingsController.java
+++ src/main/java/no/hvl/dat108/Oblig4_gr99/controllers/PaameldingsController.java
@@ -1,11 +1,7 @@
 package no.hvl.dat108.Oblig4_gr99.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PaameldingsController {
